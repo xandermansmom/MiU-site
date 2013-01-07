d705826b29b12aa674235ef318c8cc97afa5c02b
Michele Laramore <xandermansmom@fullsail.edu>
2013-01-07 16:22:17 -0600

    First Commit

---
+++ js/json.js
@@ -0,0 +1,52 @@
+var json = {
+	
+	"occasion1": {
+		"fname":			 ["First Name:", "Robert"],
+		"lname":			 [ "Last Name:", "McPhalen" ],
+		"email":			 [ "Email:", "liquidbraino@hotmail.com" ],
+		"telephone":		 [ "Telephone:", "817-567-8910" ],
+		"address":			 [ "Address:", "1515 Funny Way" ],
+		"city":				 ["City:" , "Sunland" ],
+		"states":			 ["State:", "California" ],
+		"zip":				 [ "Zip:", "87654"],
+		"occasions":		 ["Occasion:", "Birthday" ],
+		"date":				 ["Date:", "10-18-1972" ],
+		"sex":				 ["Sex:", "Male"],
+		"spend":			 ["Spend:", "100"],
+		"comments":			 ["Comments:", "Gift card" ]	
+		
+	},
+	"occasion2": {
+		"fname":			 [ "First Name:", "Chris"],
+		"lname":			 [ "Last Name:", "Jordan"],
+		"email":			 [ "Email:", "cjordan@gmail.com"],
+		"telephone":		 [ "Telephone:", "978-274-6877"],
+		"address":			 [ "Address:", "3917 Paradise Island Rd"],
+		"city":				 ["City:" , "Bellmead" ],
+		"states":			 ["State:", "Texas"],
+		"zip":				 [ "Zip:", "76705"],
+		"occasions":		 ["Occasion:", "Anniversary"],
+		"date":				 ["Date:", "09-04-2011"],
+		"sex":				 ["Sex:", "Male" ],
+		"spend":			 ["Spend:", "100"],
+		"comments":			 ["Comments:", "Bongos" ]	
+		
+	},
+	"occasion3": {
+		"fname":			 [ "First Name:", "Michele"],
+		"lname":			 [ "Last Name:", "Laramore"],
+		"email":			 [ "Email:", "xandermansmom@aol.com"],
+		"telephone":		 [ "Telephone:", "254-366-0084"],
+		"address":			 [ "Address:", "3917 Paradise Island Rd"],
+		"city":				 ["City:" , "Bellmead" ],
+		"states":			 ["State:", "Texas"],
+		"zip":				 [ "Zip:", "76705"],
+		"occasions":		 ["Occasion:", "Graduation"],
+		"date":				 ["Date:", "05-05-2014"],
+		"sex":				 ["Sex:", "Female" ],
+		"spend":			 ["Spend:", "100"],
+		"comments":			 ["Comments:", "Gift Certificate" ]	
+				
+	}
+	
+}
