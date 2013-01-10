2d4745cce82d6c49545ef61f1c5e96fba67d78a3
Michele Laramore <xandermansmom@fullsail.edu>
2013-01-10 14:50:23 -0600

    Add changes
    Conflicts:
    Week 1/.DS_Store
    Week 1/images/.DS_Store
    Week 1/images/button.gif
    Week 1/index.html
    Week 1/stylesheet/style.css

--- js/json.js
+++ js/json.js
@@ -47,6 +47,284 @@ var json = {
 		"spend":			 ["Spend:", "100"],
 		"comments":			 ["Comments:", "Gift Certificate" ]	
 				
+	},
+	
+		"occasion4": {
+		"fname":			 [ "First Name:", "Gloria"],
+		"lname":			 [ "Last Name:", "McDaniel"],
+		"email":			 [ "Email:", "gloworm@aol.com"],
+		"telephone":		 [ "Telephone:", "254-279-3637"],
+		"address":			 [ "Address:", "126 Rhode Drive"],
+		"city":				 ["City:" , "Mt. Calm" ],
+		"states":			 ["State:", "Texas"],
+		"zip":				 [ "Zip:", "76673"],
+		"occasions":		 ["Occasion:", "Anniversary"],
+		"date":				 ["Date:", "03-10-1974"],
+		"sex":				 ["Sex:", "Female" ],
+		"spend":			 ["Spend:", "100"],
+		"comments":			 ["Comments:", "Gift Certificate" ]	
+				
+	},
+	
+		"occasion5": {
+		"fname":			 [ "First Name:", "Shirley"],
+		"lname":			 [ "Last Name:", "McPhalen"],
+		"email":			 [ "Email:", "ShirleyM@yahoo.com"],
+		"telephone":		 [ "Telephone:", "254-732-6837"],
+		"address":			 [ "Address:", "4200 N. 18th #318"],
+		"city":				 ["City:" , "Waco" ],
+		"states":			 ["State:", "Texas"],
+		"zip":				 [ "Zip:", "76708"],
+		"occasions":		 ["Occasion:", "Birthday"],
+		"date":				 ["Date:", "10-18-1946"],
+		"sex":				 ["Sex:", "Female" ],
+		"spend":			 ["Spend:", "50"],
+		"comments":			 ["Comments:", "Birthday Dinner" ]	
+				
+	},
+	
+		"occasion6": {
+		"fname":			 [ "First Name:", "Gladys"],
+		"lname":			 [ "Last Name:", "Knight"],
+		"email":			 [ "Email:", "GKPips@aol.com"],
+		"telephone":		 [ "Telephone:", "818-733-2890"],
+		"address":			 [ "Address:", "37 Pirate Cove"],
+		"city":				 ["City:" , "Los Angeles" ],
+		"states":			 ["State:", "California"],
+		"zip":				 [ "Zip:", "90017"],
+		"occasions":		 ["Occasion:", "Birthday"],
+		"date":				 ["Date:", "05-28-1944"],
+		"sex":				 ["Sex:", "Female" ],
+		"spend":			 ["Spend:", "100"],
+		"comments":			 ["Comments:", "Gift Certificate" ]	
+				
+	},
+	
+		"occasion7": {
+		"fname":			 [ "First Name:", "Masi"],
+		"lname":			 [ "Last Name:", "Oka"],
+		"email":			 [ "Email:", "Yata@aol.com"],
+		"telephone":		 [ "Telephone:", "818-299-3678"],
+		"address":			 [ "Address:", "26 Sunnyvale Rd"],
+		"city":				 ["City:" , "Hollywood" ],
+		"states":			 ["State:", "California"],
+		"zip":				 [ "Zip:", "90027"],
+		"occasions":		 ["Occasion:", "Anniversary"],
+		"date":				 ["Date:", "09-01-1996"],
+		"sex":				 ["Sex:", "Male" ],
+		"spend":			 ["Spend:", "40"],
+		"comments":			 ["Comments:", "Shirt" ]	
+				
+	},
+	
+		"occasion8": {
+		"fname":			 [ "First Name:", "Cliff"],
+		"lname":			 [ "Last Name:", "Side"],
+		"email":			 [ "Email:", "CSide@msn.com"],
+		"telephone":		 [ "Telephone:", "480-344-2769"],
+		"address":			 [ "Address:", "4895 Griffin Street"],
+		"city":				 ["City:" , "Phoenix" ],
+		"states":			 ["State:", "Arizona"],
+		"zip":				 [ "Zip:", "85012"],
+		"occasions":		 ["Occasion:", "Birthday"],
+		"date":				 ["Date:", "07-06-1982"],
+		"sex":				 ["Sex:", "Male" ],
+		"spend":			 ["Spend:", "20"],
+		"comments":			 ["Comments:", "Tie" ]	
+				
+	},
+	"occasion9": {
+		"fname":			 [ "First Name:", "Michael"],
+		"lname":			 [ "Last Name:", "Reed"],
+		"email":			 [ "Email:", "MReed@aol.com"],
+		"telephone":		 [ "Telephone:", "616-378-0000"],
+		"address":			 [ "Address:", "4454 Cottonwood Lane"],
+		"city":				 ["City:" , "Grand Rapids" ],
+		"states":			 ["State:", "Michigan"],
+		"zip":				 [ "Zip:", "49505"],
+		"occasions":		 ["Occasion:", "Anniversary"],
+		"date":				 ["Date:", "06-27-1982"],
+		"sex":				 ["Sex:", "Male" ],
+		"spend":			 ["Spend:", "10"],
+		"comments":			 ["Comments:", "Anniversary Card" ]	
+				
+	},
+		"occasion10": {
+		"fname":			 [ "First Name:", "Albert"],
+		"lname":			 [ "Last Name:", "Banks"],
+		"email":			 [ "Email:", "ABanks@yahoo.com"],
+		"telephone":		 [ "Telephone:", "919-788-2345"],
+		"address":			 [ "Address:", "4232 Dola Mine Road"],
+		"city":				 ["City:" , "Raleigh" ],
+		"states":			 ["State:", "North Carolina"],
+		"zip":				 [ "Zip:", "27603"],
+		"occasions":		 ["Occasion:", "Graduation"],
+		"date":				 ["Date:", "05-01-2013"],
+		"sex":				 ["Sex:", "Male" ],
+		"spend":			 ["Spend:", "10"],
+		"comments":			 ["Comments:", "Gift Certificate" ]	
+				
+	},
+		"occasion11": {
+		"fname":			 [ "First Name:", "Doreen"],
+		"lname":			 [ "Last Name:", "Collier"],
+		"email":			 [ "Email:", "DCollier@gmail.com"],
+		"telephone":		 [ "Telephone:", "919-776-0909"],
+		"address":			 [ "Address:", "4559 Stratford Court"],
+		"city":				 ["City:" , "Cary" ],
+		"states":			 ["State:", "North Carolina"],
+		"zip":				 [ "Zip:", "27513"],
+		"occasions":		 ["Occasion:", "Birth"],
+		"date":				 ["Date:", "10-11-2012"],
+		"sex":				 ["Sex:", "Female" ],
+		"spend":			 ["Spend:", "100"],
+		"comments":			 ["Comments:", "Bassinet" ]	
+				
+	},
+	
+		"occasion12": {
+		"fname":			 [ "First Name:", "Brendan"],
+		"lname":			 [ "Last Name:", "Houston"],
+		"email":			 [ "Email:", "BHouston@yahoo.com"],
+		"telephone":		 [ "Telephone:", "502-393-8787"],
+		"address":			 [ "Address:", "3662 Earnhardt Drive"],
+		"city":				 ["City:" , "Louisville" ],
+		"states":			 ["State:", "Kentucky"],
+		"zip":				 [ "Zip:", "40220"],
+		"occasions":		 ["Occasion:", "Birthday"],
+		"date":				 ["Date:", "01-04-1989"],
+		"sex":				 ["Sex:", "Male" ],
+		"spend":			 ["Spend:", "10"],
+		"comments":			 ["Comments:", "Birthday Card" ]	
+				
+	},
+		"occasion13": {
+		"fname":			 [ "First Name:", "Ida"],
+		"lname":			 [ "Last Name:", "Zimmerman"],
+		"email":			 [ "Email:", "IdaZ@aol.com"],
+		"telephone":		 [ "Telephone:", "270-666-3976"],
+		"address":			 [ "Address:", "1897 Crosswind Drive"],
+		"city":				 ["City:" , "Henderson" ],
+		"states":			 ["State:", "Kentucky"],
+		"zip":				 [ "Zip:", "42420"],
+		"occasions":		 ["Occasion:", "Anniversary"],
+		"date":				 ["Date:", "10-07-1999"],
+		"sex":				 ["Sex:", "Female" ],
+		"spend":			 ["Spend:", "40"],
+		"comments":			 ["Comments:", "Restaurant Gift Certificate" ]	
+				
+	},
+		"occasion14": {
+		"fname":			 [ "First Name:", "Anthony"],
+		"lname":			 [ "Last Name:", "Reese"],
+		"email":			 [ "Email:", "TonyReese2014@fullsail.edu"],
+		"telephone":		 [ "Telephone:", "760-567-8910"],
+		"address":			 [ "Address:", "4993 Coleman Avenue"],
+		"city":				 ["City:" , "Vista" ],
+		"states":			 ["State:", "California"],
+		"zip":				 [ "Zip:", "92083"],
+		"occasions":		 ["Occasion:", "Christmas"],
+		"date":				 ["Date:", "12-25-2012"],
+		"sex":				 ["Sex:", "Male" ],
+		"spend":			 ["Spend:", "10"],
+		"comments":			 ["Comments:", "Gift Card" ]	
+				
+	},
+		"occasion15": {
+		"fname":			 [ "First Name:", "Pete"],
+		"lname":			 [ "Last Name:", "Cruz"],
+		"email":			 [ "Email:", "PCruz@yahoo.com"],
+		"telephone":		 [ "Telephone:", "818-210-2110"],
+		"address":			 [ "Address:", "3120 Red Maple Drive"],
+		"city":				 ["City:" , "Los Angeles" ],
+		"states":			 ["State:", "California"],
+		"zip":				 [ "Zip:", "90017"],
+		"occasions":		 ["Occasion:", "Birthday"],
+		"date":				 ["Date:", "11-19-1967"],
+		"sex":				 ["Sex:", "Male" ],
+		"spend":			 ["Spend:", "20"],
+		"comments":			 ["Comments:", "Gift Certificate" ]	
+				
+	},
+		"occasion16": {
+		"fname":			 [ "First Name:", "Dorothy"],
+		"lname":			 [ "Last Name:", "Fuller"],
+		"email":			 [ "Email:", "DFuller@aol.com"],
+		"telephone":		 [ "Telephone:", "805-344-9876"],
+		"address":			 [ "Address:", "2715 Creekside Lane"],
+		"city":				 ["City:" , "Ventura" ],
+		"states":			 ["State:", "California"],
+		"zip":				 [ "Zip:", "93001"],
+		"occasions":		 ["Occasion:", "Birth"],
+		"date":				 ["Date:", "12-10-11"],
+		"sex":				 ["Sex:", "Female" ],
+		"spend":			 ["Spend:", "15"],
+		"comments":			 ["Comments:", "Bottles" ]	
+				
+	},
+		"occasion17": {
+		"fname":			 [ "First Name:", "Katherine"],
+		"lname":			 [ "Last Name:", "Payne"],
+		"email":			 [ "Email:", "KatLady@gmail.com"],
+		"telephone":		 [ "Telephone:", "971-202-9634"],
+		"address":			 [ "Address:", "4519 Pratt Avenue"],
+		"city":				 ["City:" , "Portland" ],
+		"states":			 ["State:", "Washington"],
+		"zip":				 [ "Zip:", "97205"],
+		"occasions":		 ["Occasion:", "Birthday"],
+		"date":				 ["Date:", "06-15-1988"],
+		"sex":				 ["Sex:", "Female" ],
+		"spend":			 ["Spend:", "10"],
+		"comments":			 ["Comments:", "Birthday Card" ]	
+				
+	},
+		"occasion18": {
+		"fname":			 [ "First Name:", "Donald"],
+		"lname":			 [ "Last Name:", "Howard"],
+		"email":			 [ "Email:", "DonHoward@aol.com"],
+		"telephone":		 [ "Telephone:", "361-476-9090"],
+		"address":			 [ "Address:", "3202 White Avenue"],
+		"city":				 ["City:" , "Corpus Christi" ],
+		"states":			 ["State:", "Texas"],
+		"zip":				 [ "Zip:", "78476"],
+		"occasions":		 ["Occasion:", "Graduation"],
+		"date":				 ["Date:", "05-01-2013"],
+		"sex":				 ["Sex:", "Male" ],
+		"spend":			 ["Spend:", "10"],
+		"comments":			 ["Comments:", "Graduate Card" ]	
+				
+	},
+		"occasion19": {
+		"fname":			 [ "First Name:", "Diane"],
+		"lname":			 [ "Last Name:", "Osborne"],
+		"email":			 [ "Email:", "LadyDi@aol.com"],
+		"telephone":		 [ "Telephone:", "218-676-0987"],
+		"address":			 [ "Address:", "3843 Terra Cotta Street"],
+		"city":				 ["City:" , "Elbow Lake" ],
+		"states":			 ["State:", "Minnesota"],
+		"zip":				 [ "Zip:", "56531"],
+		"occasions":		 ["Occasion:", "Anniversary"],
+		"date":				 ["Date:", "09-04-1981"],
+		"sex":				 ["Sex:", "Female" ],
+		"spend":			 ["Spend:", "50"],
+		"comments":			 ["Comments:", "Restaurant Gift Card" ]	
+				
+	},
+		"occasion20": {
+		"fname":			 [ "First Name:", "Rosalie"],
+		"lname":			 [ "Last Name:", "Adams"],
+		"email":			 [ "Email:", "RoseAdams@gmail.com"],
+		"telephone":		 [ "Telephone:", "409-766-4534"],
+		"address":			 [ "Address:", "1806 Glenview Drive"],
+		"city":				 ["City:" , "Victoria" ],
+		"states":			 ["State:", "Texas"],
+		"zip":				 [ "Zip:", "77901"],
+		"occasions":		 ["Occasion:", "Christmas"],
+		"date":				 ["Date:", "12-25-2012"],
+		"sex":				 ["Sex:", "Female" ],
+		"spend":			 ["Spend:", "30"],
+		"comments":			 ["Comments:", "Gift Certificate" ]	
+				
 	}
 	
 }
