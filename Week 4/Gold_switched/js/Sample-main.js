ca7b5e7d2b291cf7a5c46032b21454de72c7ddab
Michele Laramore <xandermansmom@fullsail.edu>
2013-01-28 15:37:55 -0600

    Got popup message for local storage. Still nothing in local storage

--- Week 4/Gold_switched/js/Sample-main.js
+++ Week 4/Gold_switched/js/Sample-main.js
@@ -23,24 +23,59 @@ $('#additem').on('pageinit', function(){
 			},
 			submitHandler: function(){
 				var data = aiform.serializeArray();
-				storeData(data);
+				storeData(this.key);
 		}
 	});
-
 });
 
-	
-//Find value of selected radio button
-	function getSelectedRadio(){
-		var radios = $("#sex");
-		for(var i=0; i<radios.length; i++){
-			if(radios[i].checked){
-				sexValue = radios[i].value;
-			}
-		}
-}
 
-var storeData = function (key){
+//Auto Populate Local Storage
+	function autoFillData(){
+		//The actual JSON OBJECT required for this to work is coming from our json.js file which is loaded from our HTML page.
+		//Store the JSON OBJECT in local storage.
+		for(var n in json){
+			var id = Math.floor(Math.random()*100000001);	
+				localStorage.setItem(id, JSON.stringify(json[n]));
+		}	
+	}
+
+function getData(){
+		toggleControls("on");
+		if(localStorage.length === 0){
+			alert("There is no data in local storage so default data was added.");
+			autoFillData();
+		}	
+	//Write data from local storage to the browser		
+	var makeDiv = document.createElement('div');
+	makeDiv.setAttribute("id", "items");
+	var makeList = document.createElement('ul');
+	makeDiv.appendChild(makeList);
+	document.body.appendChild(makeDiv);
+	$('items').style.display = "block";		
+	for(var i=0, len=localStorage.length; i<len; i++){
+		var makeLi = document.createElement('li');
+		var linksLi = document.createElement('li');
+		makeList.appendChild(makeLi);
+		var key = localStorage.key(i);
+		var value = localStorage.getItem(key);		
+		//Convert the string in local storage back into an object using JSON.parse method
+		var obj =JSON.parse(value);	
+		var makeSubList = document.createElement('ul');
+		makeLi.appendChild(makeSubList);
+		getImage(obj.occasions[1], makeSubList);
+		for(var n in obj){
+			var makeSubLi = document.createElement('li');
+			makeSubList.appendChild(makeSubLi);
+			var optSubText = obj[n][0]+" "+obj[n][1];
+			makeSubLi.innerHTML = optSubText;
+			makeSubList.appendChild(linksLi);
+			}
+			makeItemLinks(localStorage.key(i), linksLi);//Create edit and delete links for each list item in local storage
+		}
+	}
+	
+
+var storeData = function(key){
 		//If there is no key, generate a new key for the brand new item
 		if(!key){
 		var id 				= Math.floor(Math.random()*100000001);	
@@ -50,36 +85,60 @@ var storeData = function (key){
 			//to the validate function and then passed here to the saveData function.
 			id = key;
 		}
-
-		//Find value of radio button
-		getSelectedRadio();
-
+		
 		//Gather form field values and store in an object
 		//Object properties contain an array with the form label and input value
 		var item 				= {};
-			item.fname			= ["First Name:", $('#fname').value];
-			item.lname			= [ "Last Name:", $('#lname').value];
-			item.email			= [ "Email:", $('#email').value];
-			item.telephone		= [ "Telephone:", $('#telephone').value];
-			item.address		= [ "Address:", $('#address').value];
-			item.city			= ["City:" , $('#city').value];
-			item.state			= ["State:", $('#state').value];
-			item.zip			= [ "Zip:", $('#zip').value];
-			item.occasion		= ["Occasion:", $('#occasion').value];
-			item.date			= ["Date:", $('#date').value];
-			item.sex			= ["Sex:", sexValue];
-			item.spend			= ["Spend:", $('#Maxspend').value];
-			item.comments		= ["Comments:", $('#comments').value];				
+			item.fname			= ["First Name", $('#fname').val()];
+			item.lname			= [ "Last Name", $('#lname').val()];
+			item.email			= [ "Email", $('#email').val()];
+			item.telephone		= [ "Telephone", $('#telephone').val()];
+			item.address		= [ "Address", $('#address').val()];
+			item.city			= ["City", $('#city').val()];
+			item.state			= ["State", $('#state').val()];
+			item.zip			= [ "Zip", $('#zip').val()];
+			item.occasion		= ["Occasion", $('#occasion').val()];
+			item.date			= ["Date", $('#date').val()];
+			item.radio			= ["Sex", $("input:radio[name=Sex]:checked").val()];
+			item.spend			= ["Spend", $('#maxSpend').val()];
+			item.comments		= ["Comments", $('#comments').val()];				
 		//Save data into local storage. Use stringify to convert objects into strings.
 		localStorage.setItem(id, JSON.stringify(item));		
 		alert("The Special Occasion is saved!");		
 	};
 
+	
+	
+	function deleteItem(){
+		var ask = confirm("Are you sure you want to delete this record?");
+		if(ask){
+			localStorage.removeItem(this.key);
+			alert("Record was deleted!");
+			window.location.reload();
+		}else{
+			alert("Record was NOT deleted.");
+		}
+	}
 
-//Reset link to clear page		
-Reset = function() {
+function clearLocal(){
+		//clear data from local storage.
+		if(localStorage.length === 0){
+			alert("There is no data to clear.");
+		}else{
+			localStorage.clear();
+			alert("All data is cleared.");
+			window.location.reload;
+			return false;
+		}		
+	}
+	
+	
+	//Reset link to clear page		
+	Reset = function() {
     var clearpage = $('#additem')[0].reset();
 };
+
+
 
 	
 
